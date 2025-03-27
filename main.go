e188b9eb7fb77dfa45b69138fe28753b0ebdf27c
naveen <naveen@playo.co>
2025-03-27 18:18:12 +0530

    changed barcode printing width

--- main.go
+++ main.go
@@ -34,15 +34,15 @@ func ConvertToESCPos(img image.Image) []byte {
 	const (
 		// printableWidth  = 384 // 2 inches (48mm) at 203 DPI
 		// printableHeight = 192 // 1 inch (24mm) at 203 DPI
-		printableWidth  = 384 // 2 inches (48mm) at 203 DPI
-		printableHeight = 155 // 1 inch (24mm) at 203 DPI
+		printableWidth  = 400 // 2 inches (48mm) at 203 DPI
+		printableHeight = 78 // 1 inch (24mm) at 203 DPI
 	)
 
 	// Resize image to exact label size to prevent cutoff & wrapping
 	img = resize.Resize(printableWidth, printableHeight, img, resize.Bicubic)
 
 	bounds := img.Bounds()
-	width := bounds.Dx() + 125
+	width := bounds.Dx() + 100
 	height := bounds.Dy()
 
 	// Calculate width in bytes (ESC/POS requires width to be in 8-pixel chunks)
